ec968ee415a2ac3687861ce9ab9e9ffb0b2224ab
alexandreponte1 <alexandre.s.ponte@concrete.com.br>
2022-07-04 12:11:53 -0300

    update

---
+++ alura/docker-elastic-beanstalk-aws/infra/variables.tf
@@ -0,0 +1,42 @@
+variable "regiao_aws" {
+  type = string
+}
+variable "chave" {
+    type = string
+}
+variable "instancia" {
+  type = string
+}
+
+variable "grupoDeSeguranca" {
+  type = string
+}
+
+
+variable "aws_profile" {
+  type        = string
+  description = ""
+  default     = "playground"
+}
+
+variable "cdirs_acesso_remoto" {
+  type    = list(string)
+  default = ["0.0.0.0./00"]
+}
+
+
+variable "minimo" {
+  type = number
+}
+
+variable "maximo" {
+  type = number
+}
+
+variable "nomeGrupo" {
+  type = string
+}
+
+variable "producao" {
+  type = bool
+}
