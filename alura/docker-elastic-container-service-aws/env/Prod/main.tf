5b707d141122235bbe43a07e96af06a68ac941ec
alexandreponte1 <uaspont@gmail.com>
2022-07-08 17:36:14 -0300

    update

---
+++ alura/docker-elastic-container-service-aws/env/Prod/main.tf
@@ -0,0 +1,11 @@
+module "prod" {
+  source           = "../../Infra"
+  nome_repositorio = "producao"
+  cargoIAM         = "producao"
+  ambiente         = "producao"
+}
+
+
+output "IP_alb" {
+  value = module.prod.IP
+}
