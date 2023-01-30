9774b58d9cc06fb7045b634eb86d159080ad20c4
alexandreponte1 <alexandre.s.ponte@concrete.com.br>
2023-01-30 16:50:21 -0300

    update

--- Associate_Certification/GURU/builInFunctions/Dynamic-Blocks.tf
+++ Associate_Certification/GURU/builInFunctions/Dynamic-Blocks.tf
@@ -42,7 +42,20 @@ resource "aws_security_group" "my-sg"{
 }
 
 
-
+variable "rules" {
+  default = [
+    {
+      port        = 80
+      proto       = "tcp"
+      cidr_blocks = ["0.0.0.0./0"]
+    },
+    {
+      port        = 22
+      proto       = "tcp"
+      cidr_blocks = ["1.2.3.4/32"]
+    }
+  ]
+}
 
 
 
