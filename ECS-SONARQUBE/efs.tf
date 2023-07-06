a39b2ffa56c0365f07420f9d45c2137657fe4571
alexandre <alexandre@pop-os.localdomain>
2023-07-05 21:34:34 -0300

    update ECS sonar

--- ECS-SONARQUBE/efs.tf
+++ ECS-SONARQUBE/efs.tf
@@ -1,15 +1,15 @@
-# resource "aws_efs_file_system" "foo" {
-#   tags = {
-#     Name = "ECS-EFS-FS"
-#   }
-# }
+resource "aws_efs_file_system" "foo" {
+  tags = {
+    Name = "ECS-EFS-FS"
+  }
+}
 
-# resource "aws_efs_mount_target" "mount" {
-#   file_system_id = aws_efs_file_system.foo.id
-#   subnet_id      = element(module.vpc.private_subnets, 0)
-#   # security_groups = [aws_security_group_rule.entrada_ECS.id, aws_security_group_rule.saida_ECS.id, aws_security_group.alb.id]
+resource "aws_efs_mount_target" "mount" {
+  file_system_id = aws_efs_file_system.foo.id
+  subnet_id      = element(module.vpc.private_subnets, 0)
+  # security_groups = [aws_security_group_rule.entrada_ECS.id, aws_security_group_rule.saida_ECS.id, aws_security_group.alb.id]
 
-#   # tags = {
-#   #   Name = "ECS-EFS-MNT"
-#   # }
-# }
+  # tags = {
+  #   Name = "ECS-EFS-MNT"
+  # }
+}
--- ECS-SONARQUBE/security_group.tf
+++ ECS-SONARQUBE/security_group.tf
@@ -43,3 +43,53 @@ resource "aws_security_group_rule" "saida_ECS" {
   cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
   security_group_id = aws_security_group.privado.id
 }
+
+
+resource "aws_security_group_rule" "entrada_EFS" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.EFS.id
+  security_group_id        = aws_security_group.privado.id
+}
+
+
+resource "aws_security_group_rule" "saida_EFS" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  # cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
+  source_security_group_id = aws_security_group.privado.id
+  security_group_id = aws_security_group.EFS.id
+}
+
+
+
+
+
+resource "aws_security_group" "EFS" {
+  name        = "EFS-SG"
+  description = "Allow incoming connections"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "teste123"
+  }
+}
