1732f1e3c8e3099a578280bfbe5441a4360d85c0
alexandreponte2 <79060274+alexandreponte2@users.noreply.github.com>
2023-07-08 11:07:47 -0300

    update ECS ip publico (#29)

--- ECS/ECS-SonarQube-IPpublico/ecs.tf
+++ ECS/ECS-SonarQube-IPpublico/ecs.tf
@@ -12,19 +12,18 @@ module "ecs" {
 }
 
 
-
-
 resource "aws_ecs_task_definition" "sonar" {
   family                   = "sonar-example"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "1024"
   memory                   = "4096"
   network_mode             = "awsvpc"
+  execution_role_arn       = aws_iam_role.ECS.arn
   container_definitions = jsonencode(
     [
       {
-        "name"      = "producao"
-        "image"     = "sonarqube"
+        "name"      = "sonarqube"
+        "image"     = "sonarqube:9.8-community"
         "cpu"       = 1024
         "memory"    = 4096
         "essential" = true
@@ -34,34 +33,40 @@ resource "aws_ecs_task_definition" "sonar" {
             "hostPort"      = 9000
           }
         ],
-      #   "mountPoints" = [
-      #   {
-      #      "sourceVolume" = "data",
-      #      "containerPath" = "/opt/sonarqube/data",
-      #      "readOnly" = false
-      #  }
-      #   ]
+          "mountPoints" = [
+          {
+             "sourceVolume" = "data",
+             "containerPath" = "/opt/sonarqube/data",
+             "readOnly" = false
+         }
+          ],
+        "logConfiguration" = {
+          "logDriver" = "awslogs",
+          "options" = {
+            "awslogs-group"         = "sonarqube",
+            "awslogs-create-group"  = "true"
+            "awslogs-region"        = "us-east-1",
+            "awslogs-stream-prefix" = "ecs"
+          }
+        }
       }
     ]
   )
-  # volume {
-  #   name = "data"
-  #   efs_volume_configuration {
-  #   file_system_id = aws_efs_file_system.foo.id
-  #   root_directory = "/"
-  #    }
-  # }
+  volume {
+    name = "data"
+    efs_volume_configuration {
+    file_system_id = "fs-0beb9ded1eb974aa6"
+    root_directory = "/"
+     }
+  }
 }
 
-
-
-
 resource "aws_ecs_service" "SonarSonarQubeQube" {
-  name            = "sonar-example"
-  cluster         = module.ecs.ecs_cluster_id
-  task_definition = aws_ecs_task_definition.sonar.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
+  name             = "sonar-example"
+  cluster          = module.ecs.ecs_cluster_id
+  task_definition  = aws_ecs_task_definition.sonar.arn
+  desired_count    = 1
+  launch_type      = "FARGATE"
   platform_version = "1.4.0" //not specfying this version explictly will not currently work for mounting EFS to Fargate
 
   network_configuration {
--- ECS/ECS-SonarQube-IPpublico/efs.tf
+++ ECS/ECS-SonarQube-IPpublico/efs.tf
@@ -1,13 +1,13 @@
 # resource "aws_efs_file_system" "foo" {
 #   tags = {
-#     Name = "ECS-EFS-FS"
+#     Name = "ECS-EFS-MNT"
 #   }
 # }
 
 # resource "aws_efs_mount_target" "mount" {
-#   file_system_id = aws_efs_file_system.foo.id
-#   subnet_id      = element(module.vpc.public_subnets, 0)
-
+#   file_system_id  = aws_efs_file_system.foo.id
+#   subnet_id       = element(module.vpc.public_subnets, 0)
+#   security_groups = ["${aws_security_group.EFS.id}"]
 #   # tags = {
 #   #   Name = "ECS-EFS-MNT"
 #   # }
