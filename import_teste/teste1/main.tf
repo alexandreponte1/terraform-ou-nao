8c464013d1f829ce03e031a13c9350089558312b
Alexandre Ponte <uaspont@gmail.com>
2023-07-25 10:28:33 -0300

    Update main.tf

--- import_teste/teste1/main.tf
+++ import_teste/teste1/main.tf
@@ -1,14 +1,4 @@
-terraform {
-
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-
-  }
-}
-
 provider "aws" {
   region  = var.aws_region
-  profile = "playground"
+  profile = var.aws_profile
 }
