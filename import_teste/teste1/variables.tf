81cc80ec14eee5000e9b6be599be4613a8e4dd8a
Alexandre Ponte <uaspont@gmail.com>
2023-07-25 10:27:56 -0300

    Update variables.tf

--- import_teste/teste1/variables.tf
+++ import_teste/teste1/variables.tf
@@ -4,9 +4,25 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable "aws_profile" {
+  type        = string
+  description = " "
+  default     = "playground"
+}
 
-# variable "dbpassword" {
-#   type        = string
-#   description = ""
-#   #  default     = "us-east-1"
-# }
+
+variable "cdirs_acesso_remoto" {
+  type    = list(string)
+  default = ["0.0.0.0/0"] # recomendo colocar o seu ip aqui /32
+}
+
+variable "ambiente" {
+  type        = string
+  description = " "
+  default     = "SonarQube"
+}
+
+variable "cargoIAM" {
+  type    = string
+  default = "producao"
+}
